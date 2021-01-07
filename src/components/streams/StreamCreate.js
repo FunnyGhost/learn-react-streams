9d2c5db3ade5d7b9d50a1eed356eb961ea79349d
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2021-01-07 17:42:10 +0100

    Style the error and only show if touched

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({input, label, meta}) {
+  renderError = ({error, touched}) => {
+    if (touched && error) {
+      return (<div className="ui error message">
+            <div className="header">
+              {error}
+            </div>
+          </div>
+      );
+    }
+  };
+
+  renderInput = ({input, label, meta}) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-        <div className="field">
+        <div className={className}>
           <label>{label}</label>
           <input {...input} />
-          <div>
-            {meta.error}
-          </div>
+          {this.renderError(meta)}
         </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -20,7 +30,7 @@ class StreamCreate extends React.Component {
 
   render() {
     return (
-        <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <Field name="title" component={this.renderInput} label="Enter title"/>
           <Field name="description" component={this.renderInput} label="Enter description"/>
           <button className="ui button primary">Submit</button>
@@ -31,15 +41,15 @@ class StreamCreate extends React.Component {
 
 const validate = (formValues) => {
   const errors = {};
-  if(!formValues.title){
-    errors.title = "Please enter a title";
+  if (!formValues.title) {
+    errors.title = 'Please enter a title';
   }
-  if(!formValues.description){
-    errors.description = "Please enter a description";
+  if (!formValues.description) {
+    errors.description = 'Please enter a description';
   }
 
   return errors;
-}
+};
 
 export default reduxForm({
   form: 'streamCreate',
