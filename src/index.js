aec2f3e93d8ed9a97267d7380d37075653dd53a5
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2021-01-11 17:51:32 +0100

    Wire up streams creation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from './components/App';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
+import  reduxThunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware()));
+    composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
