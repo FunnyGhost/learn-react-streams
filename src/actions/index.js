3155b7525cf673d92768f3f83ab465dff4391c78
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2021-02-03 18:10:31 +0100

    Redirect after Edit  or Delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -46,10 +46,12 @@ export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({type: EDIT_STREAM, payload: response.data});
+  history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({type: DELETE_STREAM, payload: id});
+  history.push('/');
 };
